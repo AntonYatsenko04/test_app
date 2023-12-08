fcd41976cb4cce1133246d01eedb6d872a345d5c
Антон Яценко <anton.yatsenko.w@gmail.com>
2023-12-08 20:25:53 +0300

    add user bloc

---
+++ lib/presentation/bloc/bloc/user_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:test_app/core/data_state.dart';
+import 'package:test_app/domain/models/user_model.dart';
+import 'package:test_app/domain/usecase/get_all_users_use_case.dart';
+
+part 'user_event.dart';
+part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final GetAllUsersUseCase _getAllUsers;
+
+  UserBloc(this._getAllUsers) : super(const UserState()) {
+    on<FetchUsersEvent>(_onReloadEvent);
+  }
+
+  _onReloadEvent(event, emit) async {
+    emit(state.copyWith(status: UserStatus.loading));
+    final usersDataState = await _getAllUsers();
+    if (usersDataState is DataSuccess) {
+      emit(state.copyWith(
+          users: usersDataState.data, status: UserStatus.onlineSuccess));
+    } else if (usersDataState is DataOffline) {
+      emit(state.copyWith(
+          users: usersDataState.data, status: UserStatus.offlineSuccess));
+    } else {
+      emit(state.copyWith(status: UserStatus.failure));
+    }
+  }
+}
---
+++ lib/presentation/bloc/bloc/user_state.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+part of 'user_bloc.dart';
+
+enum UserStatus { initial, loading, onlineSuccess, offlineSuccess, failure }
+
+final class UserState extends Equatable {
+  const UserState(
+      {this.users = const <UserModel>[], this.status = UserStatus.initial});
+
+  final List<UserModel> users;
+  final UserStatus status;
+
+  UserState copyWith({
+    List<UserModel>? users,
+    UserStatus? status,
+  }) {
+    return UserState(
+      users: users ?? this.users,
+      status: status ?? this.status,
+    );
+  }
+
+  @override
+  List<Object> get props => [users, status];
+
+  @override
+  bool get stringify => true;
+}
