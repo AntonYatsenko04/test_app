8d06d0f3da35cf4f0397b282679caaff3edd25d4
Антон Яценко <anton.yatsenko.w@gmail.com>
2023-12-20 16:39:01 +0300

    move userRepoImpl to data

--- lib/presentation/bloc/bloc/user_state.dart
+++ lib/presentation/bloc/bloc/user_state.dart
@@ -5,8 +5,10 @@ part of 'user_bloc.dart';
 enum UserStatus { initial, loading, onlineSuccess, offlineSuccess, failure }
 
 final class UserState extends Equatable {
-  const UserState(
-      {this.users = const <UserModel>[], this.status = UserStatus.initial});
+  const UserState({
+    this.users = const <UserModel>[],
+    this.status = UserStatus.initial,
+  });
 
   final List<UserModel> users;
   final UserStatus status;
